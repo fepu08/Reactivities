9b0304fea599c207d54be46f23970c7fd49d7218
fepu08 <ferencbesenyei@protonmail.com>
2021-03-19 15:57:59 +0100

    fetching data from the API

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const App = () => {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    });
+  }, [])
+
   return (
     <div className='App'>
       <h1>Hello World!</h1>
+      <ul>
+        {
+          activities.map((activity: any) => {
+            <li key={activity.id}>
+              {activity.title}
+            </li>
+          })
+        }
+      </ul>
     </div>
   );
 };
