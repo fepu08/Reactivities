2460a7139ff7726378c13c5fa9edba92e522c661
fepu08 <ferencbesenyei@protonmail.com>
2021-03-21 20:13:26 +0100

    adding a create handler

--- backend/API/Controllers/ActivitiesController.cs
+++ backend/API/Controllers/ActivitiesController.cs
@@ -10,24 +10,22 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
 
-        /**
-        *   @desc   get all activities 
-        *   @route  /activities
-        */
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
             return await Mediator.Send(new List.Query());
         }
 
-        /**
-        *   @desc   get single activity by id
-        *   @route  /activities/id
-        */
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             return await Mediator.Send(new Details.Query { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
         }
     }
 }
