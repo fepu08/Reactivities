cd82428b427ee85799207c3cebc1fcc9ed6777b3
fepu08 <ferencbesenyei@protonmail.com>
2021-03-23 08:21:19 +0100

    adding an edit handler

--- backend/API/Controllers/ActivitiesController.cs
+++ backend/API/Controllers/ActivitiesController.cs
@@ -23,9 +23,16 @@ namespace API.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreateActivity(Activity activity)
+        public async Task<IActionResult> CreateActivity(Activity activity)
         {
             return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command { Activity = activity }));
         }
     }
 }
