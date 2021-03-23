f00ba166cb11fa1bdc1487d4d7f21b68168ae8f5
fepu08 <ferencbesenyei@protonmail.com>
2021-03-23 13:34:38 +0100

    Creating an activity dashboard

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,31 +1,28 @@
-import React, {useState, useEffect, Fragment} from "react";
-import axios from 'axios';
-import {Container, Header, List} from 'semantic-ui-react'
+import React, { useState, useEffect, Fragment } from "react";
+import axios from "axios";
+import { Container, Header, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
-import './styles.css'
+import "./styles.css";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 const App = () => {
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        setActivities(response.data);
+      });
     // eslint-disable-next-line
-  }, [])
+  }, []);
 
   return (
     <Fragment>
-      <NavBar/>
-      <Container style={{marginTop: '7em'}}>
-      <List>
-        {activities.map((activity) => (
-          <List.Item key={activity.id}>
-            {activity.title}
-          </List.Item>
-        ))}
-      </List>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard activities={activities} />
       </Container>
     </Fragment>
   );
