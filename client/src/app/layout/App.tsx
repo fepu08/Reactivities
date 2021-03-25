a8539f92737e56ce58912d4b5afe7c2cc57a28f5
fepu08 <ferencbesenyei@protonmail.com>
2021-03-25 12:35:52 +0100

    adding routes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,28 +1,21 @@
-import React, { useEffect, Fragment } from "react";
+import React, { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
 import "./styles.css";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
+import HomePage from "../../features/home/HomePage";
+import { Route } from "react-router-dom";
+import ActivityForm from "../../features/activities/form/ActivityForm";
 
 const App = () => {
-  const { activityStore } = useStore();
-
-  useEffect(() => {
-    activityStore.loadActivities();
-    // eslint-disable-next-line
-  }, [activityStore]);
-
-  if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading app" />;
-
   return (
     <Fragment>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <ActivityDashboard />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/activities" component={ActivityDashboard} />
+        <Route path="/createActivity" component={ActivityForm} />
       </Container>
     </Fragment>
   );
---
+++ client/src/features/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const HomePage = () => {
+  return (
+    <Container style={{ marginTop: "7em" }}>
+      <h2>Home Page</h2>
+    </Container>
+  );
+};
+
+export default HomePage;
