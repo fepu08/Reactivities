85327e650d6184ab328f125bbfe73589cee9ebe6
fepu08 <ferencbesenyei@protonmail.com>
2021-03-31 15:45:38 +0200

    Using Date-FNS

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -1,7 +1,7 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import agent from "../api/agent";
 import { Activity } from "../models/activity";
-
+import { format } from "date-fns";
 export default class ActivityStore {
   activities: Activity[] = [];
   activityRegistry = new Map<string, Activity>();
@@ -23,7 +23,7 @@ export default class ActivityStore {
   get groupedActivities() {
     return Object.entries(
       this.activitiesByDate.reduce((activities, activity) => {
-        const date = activity.date.toISOString().split("T")[0];
+        const date = format(activity.date!, "dd MMM yyyy");
         activities[date] = activities[date]
           ? [...activities[date], activity]
           : [activity];
--- client/src/features/activities/details/ActivityDetailedInfo.tsx
+++ client/src/features/activities/details/ActivityDetailedInfo.tsx
@@ -2,7 +2,7 @@ import { observer } from "mobx-react-lite";
 import React from "react";
 import { Segment, Grid, Icon } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
-
+import { format } from "date-fns";
 interface Props {
   activity: Activity;
 }
@@ -26,7 +26,7 @@ const ActivityDetailedInfo = ({ activity }: Props) => {
             <Icon name="calendar" size="large" color="teal" />
           </Grid.Column>
           <Grid.Column width={15}>
-            <span>{activity.date}</span>
+            <span>{format(activity.date!, "dd MMM yyyy h:mm aa")}</span>
           </Grid.Column>
         </Grid>
       </Segment>
