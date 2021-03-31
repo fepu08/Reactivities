70afb144ed02b6f3e3a31f20728718f4aaa1ecf0
fepu08 <ferencbesenyei@protonmail.com>
2021-03-31 11:14:57 +0200

    handling 500 errors on the client

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -11,6 +11,7 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import { ToastContainer } from "react-toastify";
 import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 const App = () => {
   const location = useLocation();
@@ -34,6 +35,7 @@ const App = () => {
                   component={ActivityForm}
                 />
                 <Route path="/errors" component={TestErrors} />
+                <Route path="/server-error" component={ServerError} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
