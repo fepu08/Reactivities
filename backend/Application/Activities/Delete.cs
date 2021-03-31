9bc4050c2c42fa01d45a02f97c3d4e73ae3ebc2c
fepu08 <ferencbesenyei@protonmail.com>
2021-03-31 10:10:22 +0200

    handling exceptions

--- backend/Application/Activities/Delete.cs
+++ backend/Application/Activities/Delete.cs
@@ -25,9 +25,8 @@ namespace Application.Activities
             {
                 var activity = await _context.Activities.FindAsync(request.Id);
 
-                if (activity == null) return null;
+                //if (activity == null) return null;
 
-                // Error handling needed
                 _context.Remove(activity);
 
                 var result = await _context.SaveChangesAsync() > 0;
