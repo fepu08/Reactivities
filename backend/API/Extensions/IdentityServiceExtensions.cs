77b553818cebdd75c682ed8b1c81c9b0d8ef8a59
fepu08 <ferencbesenyei@protonmail.com>
2021-04-01 15:44:07 +0200

    creating a token service

--- backend/API/Extensions/IdentityServiceExtensions.cs
+++ backend/API/Extensions/IdentityServiceExtensions.cs
@@ -1,3 +1,4 @@
+using API.Services;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
@@ -18,6 +19,7 @@ namespace API.Extensions
            .AddSignInManager<SignInManager<AppUser>>();
 
            services.AddAuthentication();
+           services.AddScoped<TokenService>();
 
            return services;
        } 
