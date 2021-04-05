7953ac687721252a1d25b27c769e1c21afa768c8
fepu08 <ferencbesenyei@protonmail.com>
2021-04-05 14:51:13 +0200

    updating the home page and nav bar

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,8 +1,13 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { Menu, Button, Container } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { Menu, Button, Container, Image, Dropdown } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
-export default function NavBar() {
+const NavBar = () => {
+  const {
+    userStore: { user, logout },
+  } = useStore();
   return (
     <Menu inverted fixed="top">
       <Container>
@@ -24,7 +29,27 @@ export default function NavBar() {
             content="Create Activity"
           />
         </Menu.Item>
+        <Menu.Item position="right">
+          <Image
+            src={user?.image || "/assets/user.png"}
+            avatar
+            spaced="right"
+          />
+          <Dropdown pointing="top left" text={user?.displayName}>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to={`/profile/${user?.username}`}
+                text="My Profile"
+                icon={"user"}
+              />
+              <Dropdown.Item onClick={logout} text={"Logout"} icon="power" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
       </Container>
     </Menu>
   );
-}
+};
+
+export default observer(NavBar);
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,8 +1,11 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
 const HomePage = () => {
+  const { userStore } = useStore();
   return (
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
@@ -15,13 +18,21 @@ const HomePage = () => {
           />
           Reactivities
         </Header>
-        <Header as="h2" inverted content="Welcome to Reactivities" />
-        <Button as={Link} to="/login" size="huge" inverted>
-          Login
-        </Button>
+        {userStore.isLoggedIn ? (
+          <>
+            <Header as="h2" inverted content="Welcome to Reactivities" />
+            <Button as={Link} to="/activities" size="huge" inverted>
+              Go to Activities!
+            </Button>
+          </>
+        ) : (
+          <Button as={Link} to="/login" size="huge" inverted>
+            Login
+          </Button>
+        )}
       </Container>
     </Segment>
   );
 };
 
-export default HomePage;
+export default observer(HomePage);
