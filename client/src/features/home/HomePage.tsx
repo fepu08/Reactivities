534420820bbfede253199eb2ad4c91b52de3b52a
fepu08 <ferencbesenyei@protonmail.com>
2021-04-06 13:50:48 +0200

    user registration on client side

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { Container, Header, Segment, Image, Button } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import LoginForm from "../users/LoginForm";
+import RegisterForm from "../users/RegisterForm";
 
 const HomePage = () => {
   const { userStore, modalStore } = useStore();
@@ -37,7 +38,7 @@ const HomePage = () => {
               Login!
             </Button>
             <Button
-              onClick={() => modalStore.openModal(<h1>Hello</h1>)}
+              onClick={() => modalStore.openModal(<RegisterForm />)}
               size="huge"
               inverted
             >
--- client/src/features/users/RegisterForm.tsx
+++ client/src/features/users/RegisterForm.tsx
@@ -1,7 +1,67 @@
+import { ErrorMessage, Form, Formik } from "formik";
+import { observer } from "mobx-react-lite";
 import React from "react";
+import { Button, Header, Label } from "semantic-ui-react";
+import MyTextInput from "../../app/common/form/MyTextInput";
+import { useStore } from "../../app/stores/store";
+import * as Yup from "yup";
 
 const RegisterForm = () => {
-  return <div></div>;
+  const { userStore } = useStore();
+  return (
+    <Formik
+      initialValues={{
+        displayName: "",
+        username: "",
+        email: "",
+        password: "",
+        error: null,
+      }}
+      onSubmit={(values, { setErrors }) =>
+        userStore.register(values).catch((error) => setErrors({ error }))
+      }
+      validationSchema={Yup.object({
+        displayName: Yup.string().required(),
+        username: Yup.string().required(),
+        email: Yup.string().required().email(),
+        password: Yup.string().required(),
+      })}
+    >
+      {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
+        <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+          <Header
+            as="h2"
+            content="Sign up to Reactivities"
+            color="teal"
+            textAlign="center"
+          />
+          <MyTextInput name="displayName" placeholder="Display Name" />
+          <MyTextInput name="username" placeholder="Username" />
+          <MyTextInput name="email" placeholder="Email" />
+          <MyTextInput name="password" type="password" placeholder="Password" />
+          <ErrorMessage
+            name="error"
+            render={() => (
+              <Label
+                style={{ marginBottom: 10 }}
+                basic
+                color="red"
+                content={errors.error}
+              />
+            )}
+          />
+          <Button
+            loading={isSubmitting}
+            positive
+            content="Register"
+            type="submit"
+            fluid
+            disabled={!isValid || !dirty || isSubmitting}
+          />
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
-export default RegisterForm;
+export default observer(RegisterForm);
