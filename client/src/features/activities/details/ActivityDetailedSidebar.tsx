443af7826b15cc5afa42b1018aa44f21dbe1e903
fepu08 <ferencbesenyei@protonmail.com>
2021-04-08 10:56:30 +0200

    conditionally rendering buttons

--- client/src/features/activities/details/ActivityDetailedSidebar.tsx
+++ client/src/features/activities/details/ActivityDetailedSidebar.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Segment, List, Label, Item, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
-import { Profile } from "../../../app/models/profile";
-import ActivityListItemAttendee from "../dashboard/ActivityListItemAttendee";
-
+import { Activity } from "../../../app/models/activity";
 interface Props {
-  attendees: Profile[];
+  activity: Activity;
 }
 
-const ActivityDetailedSidebar = ({ attendees }: Props) => {
+const ActivityDetailedSidebar = ({ activity: { attendees, host } }: Props) => {
+  if (!attendees) return null;
   return (
     <>
       <Segment
@@ -26,13 +25,15 @@ const ActivityDetailedSidebar = ({ attendees }: Props) => {
         <List relaxed divided>
           {attendees.map((attendee) => (
             <Item style={{ position: "relative" }} key={attendee.username}>
-              <Label
-                style={{ position: "absolute" }}
-                color="orange"
-                ribbon="right"
-              >
-                Host
-              </Label>
+              {attendee.username === host.username && (
+                <Label
+                  style={{ position: "absolute" }}
+                  color="orange"
+                  ribbon="right"
+                >
+                  Host
+                </Label>
+              )}
               <Image size="tiny" src={attendee.image || "/assets/user.png"} />
               <Item.Content verticalAlign="middle">
                 <Item.Header as="h3">
