1969d25685d8a6c80bf20cb04c5d6943020051fd
fepu08 <ferencbesenyei@protonmail.com>
2021-04-11 14:31:42 +0200

    creating a photo upload widget

--- client/src/features/profiles/ProfilePhotos.tsx
+++ client/src/features/profiles/ProfilePhotos.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
 import { Card, Header, Tab, Image, Grid, Button } from "semantic-ui-react";
+import PhotoUploadWidget from "../../app/common/imageUpload/PhotoUploadWidget";
 import { Profile } from "../../app/models/profile";
 import { useStore } from "../../app/stores/store";
 
@@ -30,7 +31,7 @@ const ProfilePhotos = ({ profile }: Props) => {
         </Grid.Column>
         <Grid.Column width={16}>
           {addPhotoMode ? (
-            <p>Photo widget goes here</p>
+            <PhotoUploadWidget />
           ) : (
             <Card.Group itemsPerRow={5}>
               {profile.photos?.map((photo) => (
