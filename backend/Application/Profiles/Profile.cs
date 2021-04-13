4a0d3bf403c710840ee3734c7121eabe044f1782
fepu08 <ferencbesenyei@protonmail.com>
2021-04-13 13:03:26 +0200

    update the profile class

--- backend/Application/Profiles/Profile.cs
+++ backend/Application/Profiles/Profile.cs
@@ -9,6 +9,11 @@ namespace Application.Profiles
         public string DisplayName { get; set; }
         public string Bio { get; set; }
         public string Image { get; set; }
+        
+        // for the current user when he want to check an another user profile that he already follows or doesn't
+        public bool Following { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
         public ICollection<Photo> Photos { get; set; }
     }
 }
--- backend/Persistence/DataContext.cs
+++ backend/Persistence/DataContext.cs
@@ -41,7 +41,7 @@ namespace Persistence
                 b.HasKey(k => new {k.ObserverId, k.TargetId});
 
                 b.HasOne(o => o.Observer)
-                    .WithMany(f => f.Following)
+                    .WithMany(f => f.Followings)
                     .HasForeignKey(o => o.ObserverId)
                     .OnDelete(DeleteBehavior.Cascade);
 
