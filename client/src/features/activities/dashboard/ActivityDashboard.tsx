d27ac8831400035303c73283bb6d9947e6c2d612
fepu08 <ferencbesenyei@protonmail.com>
2021-04-14 16:03:59 +0200

    adding vertical paging

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,27 +1,49 @@
 import { observer } from "mobx-react-lite";
-import React, { useEffect } from "react";
-import { Grid } from "semantic-ui-react";
+import React, { useEffect, useState } from "react";
+import { Button, Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { PagingParams } from "../../../app/models/pagination";
 import { useStore } from "../../../app/stores/store";
 import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 const ActivityDashboard = () => {
   const { activityStore } = useStore();
-  const { loadActivities, loadingInitial, activityRegistry } = activityStore;
+  const {
+    loadActivities,
+    loadingInitial,
+    activityRegistry,
+    setPagingParams,
+    pagination,
+  } = activityStore;
+  const [loadingNext, setLoadingNext] = useState(false);
+
+  function handleGetNext() {
+    setLoadingNext(true);
+    setPagingParams(new PagingParams(pagination!.currentPage + 1));
+    loadActivities().then(() => setLoadingNext(false));
+  }
 
   useEffect(() => {
     if (activityRegistry.size <= 1) loadActivities();
     // eslint-disable-next-line
   }, [activityRegistry.size, loadActivities]);
 
-  if (loadingInitial)
+  if (loadingInitial && !loadingNext)
     return <LoadingComponent content="Loading activities..." />;
 
   return (
     <Grid>
       <Grid.Column width="10">
         <ActivityList />
+        <Button
+          floated="right"
+          content="More..."
+          positive
+          onClick={handleGetNext}
+          loading={loadingNext}
+          disabled={pagination?.totalPages === pagination?.currentPage}
+        />
       </Grid.Column>
       <Grid.Column width="6">
         <ActivityFilters />
