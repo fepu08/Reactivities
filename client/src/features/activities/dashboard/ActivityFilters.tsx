58be57a5d06e0825180619a0977a4aa2c8e828df
fepu08 <ferencbesenyei@protonmail.com>
2021-04-14 17:11:15 +0200

    updating the filter component

--- client/src/features/activities/dashboard/ActivityFilters.tsx
+++ client/src/features/activities/dashboard/ActivityFilters.tsx
@@ -1,20 +1,41 @@
+import { observer } from "mobx-react-lite";
 import React, { Fragment } from "react";
 import Calendar from "react-calendar";
 import { Header, Menu } from "semantic-ui-react";
+import { useStore } from "../../../app/stores/store";
 
 const ActivityFilters = () => {
+  const {
+    activityStore: { predicate, setPredicate },
+  } = useStore();
+
   return (
     <Fragment>
       <Menu vertical size="large" style={{ width: "100%", marginTop: 25 }}>
         <Header icon="filter" attached color="teal" content="Filters" />
-        <Menu.Item content="All Activities" />
-        <Menu.Item content="I'm going" />
-        <Menu.Item content="I'm hosing" />
+        <Menu.Item
+          content="All Activities"
+          active={predicate.has("all")}
+          onClick={() => setPredicate("all", "true")}
+        />
+        <Menu.Item
+          content="I'm going"
+          active={predicate.has("isGoing")}
+          onClick={() => setPredicate("isGoing", "true")}
+        />
+        <Menu.Item
+          content="I'm hosing"
+          active={predicate.has("isHost")}
+          onClick={() => setPredicate("isHost", "true")}
+        />
       </Menu>
       <Header />
-      <Calendar />
+      <Calendar
+        onChange={(date) => setPredicate("startDate", date as Date)}
+        value={predicate.get("startDate") || new Date()}
+      />
     </Fragment>
   );
 };
 
-export default ActivityFilters;
+export default observer(ActivityFilters);
